61148e7a52043c6f9eb160de7bd8d4e31e3dbb2a
SY <r38411@yahoo.com>
2020-06-15 17:39:24 +0800

    finish assg2b

---
+++ Java Programming Arrays/Week 2/HashMap Project/Codon.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of Codon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import edu.duke.*;
+
+public class Codon {
+    private HashMap<String, Integer> codonMap;
+    
+    public Codon() {
+        codonMap = new HashMap<String, Integer>();
+    }
+    
+    public void buildCodonMap(int start, String dna) {
+        int curIndex = start;
+        dna = dna.trim();
+        while (curIndex + 3 <= dna.length()) {
+            String code = dna.substring(curIndex, curIndex+3);
+            if (codonMap.containsKey(code)) {
+                codonMap.put(code, codonMap.get(code)+1);
+            }
+            else {
+                codonMap.put(code, 1);
+            }
+            curIndex += 3;
+        }
+    }
+    
+    public String getMostCommonCodon() {
+        String most = "";
+        int maxCount = 0;
+        for (String code : codonMap.keySet()) {
+            if (codonMap.get(code) > maxCount) {
+                most = code;
+                maxCount = codonMap.get(code);
+            }
+        }
+        return most;
+    }
+    
+    public void printCodonCounts(int start, int end) {
+        System.out.println("Counts of codons between " +start + " and " +
+                    end +" inclusive are:");
+        for (String code : codonMap.keySet()) {
+            int count = codonMap.get(code);
+            if ((count >= start) && (count <= end))
+                System.out.println(code +" : " + count);
+        }
+    }
+    
+    public void testCodon() {
+        FileResource fr = new FileResource();
+        String dna = fr.asString().toUpperCase();
+        int start = 1, end = 5;
+        for (int k=0; k<3; k++) {
+            codonMap.clear();
+            buildCodonMap(k, dna);
+            System.out.println("Reading frame starting with " + k +
+                            " results in " +codonMap.size() +" unique codons");
+            String mostCommon = getMostCommonCodon();
+            System.out.println("and most common codon is: " + mostCommon +
+                    " with count " +codonMap.get(mostCommon));
+            printCodonCounts(start, end);
+        }
+    }
+}
---
+++ Java Programming Arrays/Week 2/HashMap Project/WordsInFiles.java
@@ -0,0 +1,86 @@
+
+/**
+ * Write a description of WordsInFiles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import edu.duke.*;
+import java.io.File;
+
+public class WordsInFiles {
+    private HashMap<String, ArrayList<String>> wordMap;
+    
+    public WordsInFiles() {
+        wordMap = new HashMap<String, ArrayList<String>>();
+    }
+    
+    private void addWordsFromFile(File f) {
+        String fname = f.getName();
+        FileResource fr = new FileResource(f);
+        for (String word : fr.words()) {
+            if (wordMap.containsKey(word)) {
+                if (!wordMap.get(word).contains(fname))
+                    wordMap.get(word).add(fname);
+            }
+            else {
+                ArrayList<String> list = new ArrayList<String>();
+                list.add(fname);
+                wordMap.put(word, list);
+            }
+        }
+    }
+    
+    public void buildWordFileMap() {
+        wordMap.clear();
+        DirectoryResource dr = new DirectoryResource();
+        for (File f: dr.selectedFiles()) {
+            addWordsFromFile(f);
+        }
+    }
+    
+    public int maxNumber() {
+        int maxCount = 0;
+        for (String word : wordMap.keySet()) {
+            int count = wordMap.get(word).size();
+            if (count > maxCount)    
+                maxCount = count;
+        }
+        return maxCount;
+    }
+    
+    public ArrayList<String> wordsInNumFiles(int number) {
+        ArrayList<String> wordList = new ArrayList<String>();
+        
+        for (String word: wordMap.keySet()) {
+            if (wordMap.get(word).size() == number)
+                wordList.add(word);
+        }
+        return wordList;
+    }
+    
+    public void printFilesIn(String word) {
+        if (wordMap.containsKey(word)) {
+            System.out.print("\"" +word + "\" appears in the files: ");
+            for (String fname : wordMap.get(word))
+                System.out.print(fname + ", ");
+        }
+        System.out.println();
+    }
+    
+    public void tester() {
+        buildWordFileMap();
+        int maxNum = maxNumber();
+        ArrayList<String> wordList = wordsInNumFiles(maxNum);
+        System.out.print("The greatest number of files a word appears in is " +
+                    maxNum +", and there are  " +wordList.size() +
+                    " such words: ");
+        for (String word : wordList)
+            System.out.print("\"" +word + "\", ");
+        System.out.println();
+        
+        for (String word : wordList)
+            printFilesIn(word);
+    }
+}
