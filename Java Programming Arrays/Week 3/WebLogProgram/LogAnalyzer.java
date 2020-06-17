24827e932384e412c130e7276905eac5c7c710c9
SY <r38411@yahoo.com>
2020-06-17 23:01:53 +0800

    finish assg3b

--- Java Programming Arrays/Week 3/WebLogProgram/LogAnalyzer.java
+++ Java Programming Arrays/Week 3/WebLogProgram/LogAnalyzer.java
@@ -12,14 +12,20 @@ import edu.duke.*;
 public class LogAnalyzer
 {
      private ArrayList<LogEntry> records;
+     private HashMap<String, Integer> uniqueIPCount;
+     private HashMap<String, ArrayList<String>> ipVisitOnDay;
      
      public LogAnalyzer() {
          records = new ArrayList<LogEntry>();
+         uniqueIPCount = new HashMap<String, Integer>();
+         ipVisitOnDay = new HashMap<String, ArrayList<String>>();
      }
         
      public void readFile(String filename) {
          FileResource fr = new FileResource(filename);
          WebLogParser logParser = new WebLogParser();
+         uniqueIPCount.clear();
+         ipVisitOnDay.clear();
          for (String line : fr.lines()) {
              LogEntry logRec = logParser.parseEntry(line);
              records.add(logRec);
@@ -32,14 +38,21 @@ public class LogAnalyzer
          }
      }
      
-     public int countUniqueIPs() {
-         ArrayList<String> uniqueIP = new ArrayList<String>();
+     public HashMap<String, Integer> countVisitsPerIP() {
          for (LogEntry entry : records) {
              String ipAddr = entry.getIpAddress();
-             if (!uniqueIP.contains(ipAddr))
-                uniqueIP.add(ipAddr);
-         }
-         return uniqueIP.size();
+             if (!uniqueIPCount.containsKey(ipAddr))
+                uniqueIPCount.put(ipAddr, 1);
+             else
+                uniqueIPCount.put(ipAddr, uniqueIPCount.get(ipAddr)+1);
+         }  
+         return uniqueIPCount;
+     }
+        
+     public int countUniqueIPs() {
+         if (uniqueIPCount.isEmpty())
+            uniqueIPCount = countVisitsPerIP();
+         return uniqueIPCount.size();
      }
      
      public void printAllHigherThanNum(int num) {
@@ -50,27 +63,117 @@ public class LogAnalyzer
          }
      }
      
-     public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
-         ArrayList<String> uniqueIP = new ArrayList<String>();
+     private void getIPVisitOnDay() {
          for (LogEntry entry : records) {
              String[] timeList = entry.getAccessTime().toString().split(" ");
              String mmmDD = timeList[1] +" " + timeList[2];
              String ipAddr = entry.getIpAddress();
-             if (mmmDD.equals(someday) && (!uniqueIP.contains(ipAddr)))
-                uniqueIP.add(ipAddr);
+             if (!ipVisitOnDay.containsKey(mmmDD)) {
+                ArrayList<String> ip = new ArrayList<String>();
+                ip.add(ipAddr);
+                ipVisitOnDay.put(mmmDD, ip);
+            }
+            else {
+                ArrayList<String> ip = ipVisitOnDay.get(mmmDD);
+                ip.add(ipAddr);
+                ipVisitOnDay.put(mmmDD, ip);
+            }
+         }    
+     }
+        
+     public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+         ArrayList<String> uniqueIPAddr = new ArrayList<String>();
+         if (ipVisitOnDay.isEmpty())
+            getIPVisitOnDay();
+         if (!ipVisitOnDay.containsKey(someday))
+            return uniqueIPAddr;
+         for (String ip : ipVisitOnDay.get(someday)) {
+             if (!uniqueIPAddr.contains(ip))
+                uniqueIPAddr.add(ip);
          }
-         return uniqueIP;
+         return uniqueIPAddr;
+     }
+     
+     public HashMap<String, ArrayList<String>> iPsForDays() {
+         if (ipVisitOnDay.isEmpty())
+            getIPVisitOnDay();
+         return ipVisitOnDay;
      }
      
      public int countUniqueIPsInRange(int low, int high) {
-         ArrayList<String> uniqueIP = new ArrayList<String>();
+         ArrayList<String> uniqueIPAddr = new ArrayList<String>();
          for (LogEntry entry : records) {
              int status = entry.getStatusCode();
              String ipAddr = entry.getIpAddress();
              if ((status >= low) && (status <= high) && 
-                    (!uniqueIP.contains(ipAddr)))
-                 uniqueIP.add(ipAddr);
+                    (!uniqueIPAddr.contains(ipAddr)))
+                 uniqueIPAddr.add(ipAddr);
          }
-         return uniqueIP.size();
+         return uniqueIPAddr.size();
+     }
+     
+     public void testDayWithMostIPVisits() {
+         if (ipVisitOnDay.isEmpty()) {
+             getIPVisitOnDay();
+         }
+         for (String day : ipVisitOnDay.keySet())
+            System.out.println("Day : " +day +" , # of IP visited "
+                            +ipVisitOnDay.get(day).size());
+     }
+        
+     public String dayWithMostIPVisits() {
+         if (ipVisitOnDay.isEmpty())
+            getIPVisitOnDay();
+         int maxVisit = 0;
+         String whichDate = "";
+         for (String day : ipVisitOnDay.keySet()) {
+             if (ipVisitOnDay.get(day).size() > maxVisit) {
+                whichDate = day;
+                maxVisit = ipVisitOnDay.get(day).size();
+            }
+         }
+         return whichDate;
+     }
+     
+     public ArrayList<String> iPsWithMostVisitsOnDay(String time) {
+         if (ipVisitOnDay.isEmpty())
+            getIPVisitOnDay();
+         ArrayList<String> ipAddr = new ArrayList<String>();
+         HashMap<String, Integer> ipCount = 
+                    new HashMap<String, Integer>();
+         if (ipVisitOnDay.containsKey(time)) {
+             ArrayList<String> ipList = ipVisitOnDay.get(time);
+             for (String ip : ipList)
+                if (!ipCount.containsKey(ip))
+                    ipCount.put(ip, 1);
+                else
+                    ipCount.put(ip, ipCount.get(ip)+1);
+             ArrayList<Integer> count = new ArrayList<>(ipCount.values());
+             Collections.sort(count, Collections.reverseOrder());
+             int maxCount = count.get(0);
+             for (String ip : ipCount.keySet())
+                if (ipCount.get(ip) == maxCount)
+                    ipAddr.add(ip);
+         }
+         return ipAddr;
+     }
+     
+     public int mostNumberVisitsByIP() {
+         if (uniqueIPCount.isEmpty())
+            uniqueIPCount = countVisitsPerIP(); 
+         
+         ArrayList<Integer> ipCount = new ArrayList<>(uniqueIPCount.values());
+         Collections.sort(ipCount, Collections.reverseOrder());
+         return ipCount.get(0);
+     }
+     
+     public ArrayList<String> iPsMostVisits() {
+         int maxNum = mostNumberVisitsByIP();
+         ArrayList<String> ipAddr = new ArrayList<String>();
+         for (String addr : uniqueIPCount.keySet()) {
+             if (uniqueIPCount.get(addr) == maxNum)
+                ipAddr.add(addr);
+         }
+         return ipAddr;
      }
 }
--- Java Programming Arrays/Week 3/WebLogProgram/Tester.java
+++ Java Programming Arrays/Week 3/WebLogProgram/Tester.java
@@ -19,33 +19,67 @@ public class Tester
     
     public void testLogAnalyzer(int testcase) {
         LogAnalyzer analyzer = new LogAnalyzer();
-        String fname = "../Testcase/short-test_log";
+        String fname = "../Testcase/weblog1_log";
         analyzer.readFile(fname);
         switch (testcase) {
             case 1 : 
                 analyzer.printAll();
                 break;
             case 2:
-                System.out.println("Number of unique IPs : " 
+                System.out.println("Case 2: Number of unique IPs : " 
                         +analyzer.countUniqueIPs());
                 break;
             case 3:
-                int num = 200;
+                int num = 400;
                 analyzer.printAllHigherThanNum(num);
                 break;
             case 4:
                 String someday = "Sep 30";
                 ArrayList<String> uniqueIP = 
                             analyzer.uniqueIPVisitsOnDay(someday);
-                System.out.println("The unique IPs visit on " +someday +":");
+                System.out.println("Case 4: The unique IPs visit on " +someday +":");
+                System.out.println("Total IPs is : " +uniqueIP.size());
                 for (String ip : uniqueIP)
                     System.out.println(ip);
                 break;
             case 5:
-                int low = 300, high = 399;
-                System.out.println("The number of unique IPs which status "
+                int low = 200, high = 299;
+                System.out.println("Case 5: The number of unique IPs which status "
                         +"code between " +low +"and " +high
                         +" : " + analyzer.countUniqueIPsInRange(low, high));
+                break;
+            case 6: // test countVisitsPerIP
+                HashMap<String, Integer> countPerIP = analyzer.countVisitsPerIP();
+                System.out.println("Case 6: " +countPerIP);
+                break;
+            case 7: // test mostNu,berVisitsByIP
+                int mostNumber = analyzer.mostNumberVisitsByIP();
+                System.out.println("Case 7: The most number visited by single IP is: "
+                        +mostNumber);
+                break;
+            case 8: // test iPsMostVisits
+                ArrayList<String> ipAddr = analyzer.iPsMostVisits();
+                System.out.println("Case 8: " +ipAddr);
+                break;
+            case 9: // test iPsForDays
+                HashMap<String, ArrayList<String>> ipForDay = 
+                                analyzer.iPsForDays();
+                for (String time : ipForDay.keySet()) {
+                    System.out.println("Case 9:  on : " +time +"IPs visited : ");
+                    System.out.println(ipForDay.get(time));
+                }
+                break;
+            case 10: // test dayWithMostIPVisits
+                System.out.println("Case 10: THe most visited date is: "
+                            +analyzer.dayWithMostIPVisits());
+                break;
+            case 11: // test iPsWithMostVisitsOnDay
+                String time = "Mar 17";
+                ArrayList<String> ipList = analyzer.iPsWithMostVisitsOnDay(time);
+                System.out.println(ipList);
+                break;
+            case 12: // output dayWIthMostIPVisits
+                analyzer.testDayWithMostIPVisits();
                 break;
         }
         
